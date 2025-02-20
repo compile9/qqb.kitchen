7707a6d33b24eb8a072ec1cdbeafb78c1ee10dad
dns <200839778+compile9@users.noreply.github.com>
2025-02-19 18:12:45 -0800

    Clean Docker environment before rebuilding

--- shell_scripts/docker-rebuild.sh
+++ shell_scripts/docker-rebuild.sh
@@ -1,4 +1,8 @@
-docker-compose down -v
-docker-compose build --no-cache
-docker-compose up
+docker container prune -f
+docker rmi -f $(docker images | grep "<none>" | awk '{print $3}')
+docker image prune -af
+docker volume prune -f
+docker compose down -v
+docker compose build --no-cache
+docker compose up -d
 
