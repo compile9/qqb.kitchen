cef5bb721075b2a0935cf5d699aef2c3d659a88a
dns <200839778+compile9@users.noreply.github.com>
2025-03-28 20:24:02 -0700

    Refactor docker rebuild script

--- scripts/shell/docker-rebuild.sh
+++ scripts/shell/docker-rebuild.sh
@@ -1,5 +1,12 @@
 docker container prune -f
-docker rmi -f $(docker images | grep "<none>" | awk '{print $3}')
+
+dangling_images=$(docker images | grep "<none>" | awk '{print $3}')
+if [ -n "$dangling_images" ]; then
+  docker rmi -f $dangling_images;
+else
+  echo "No dangling images to remove."
+fi
+
 docker image prune -af
 docker volume prune -f
 docker compose down -v
